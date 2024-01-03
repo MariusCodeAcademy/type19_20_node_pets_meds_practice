b00d6830c0ed8a7cc7b079375cc20aba25fa2c0c
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-01-03 09:42:32 +0200

    get all pets

---
+++ src/routes/v1/medsRoutes.js
@@ -0,0 +1,5 @@
+// create router
+
+// /v1/api/meds/
+// GET paduos visus vaistus iš 'medications' db;
+// POST įrašys vieną vaistą į 'medications' db.
--- src/routes/v1/petsRoutes.js
+++ src/routes/v1/petsRoutes.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const { dbQueryWithData } = require('../../helper');
 
 const petsRouter = express.Router();
+
+const tableName = 'pets';
 
 // GET /v1/api/pets - grazina visus gyvunus
 petsRouter.get('/pets', async (req, res) => {
   // parsiusti ir grazinti gyvunus is db
-  res.json('grazina visus gyvunus');
+  const sql = `SELECT * FROM ${tableName} WHERE isArchived=0`;
+  const [petsArr, error] = await dbQueryWithData(sql);
+
+  if (error) {
+    console.log('error ===', error);
+    res.status(500).json('Server error');
+    return;
+  }
+
+  res.json(petsArr);
 });
 
 // POST /v1/api/pets įrašo vieną augintinį į 'pets' db;
